6f31102fade5bb4c5e9dc5f252c2e447fdaef5ad
MrMouse <mrmousese@gmail.com>
2020-01-26 01:30:14 +0400

    -add: AI draft realization;

---
+++ Assets/scripts/AI/AIController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using Player;
+using UnityEngine;
+
+namespace AI
+{
+    public class AIController : MonoBehaviour
+    {
+        public Animator Animator;
+        public PlayerController PlayerController;
+        public AIVisualSignalSystem VisualSignalSystem;
+
+        private int _actionIndex;
+
+        private void Start()
+        {
+            _actionIndex = Animator.StringToHash("ActionIndex");
+            NewActionStart();
+        }
+
+        private void NewActionStart()
+        {
+            if (PlayerController.Cooldown) StartCoroutine(DelayAfterAction(1f));
+            
+            PrepareToAction();
+        }
+
+        private void PrepareToAction()
+        {
+            var actionIndex = ChoseTheAction();
+            VisualSignalSystem.PlaceAttackSignal(actionIndex);
+            var time = 1f;
+            StartCoroutine(DelayBeforeAction(time, actionIndex));
+        }
+
+        private int ChoseTheAction()
+        {
+            return Random.Range(1, 7);
+        }
+
+        private IEnumerator DelayBeforeAction(float time, int actionIndex)
+        {
+            yield return new WaitForSeconds(time);
+            DoAction(actionIndex);
+        }
+
+        private void DoAction(int actionIndex)
+        {
+            Animator.SetInteger(_actionIndex, actionIndex);
+            PlayerController.CheckPlayerAction(actionIndex);
+            StartCoroutine(DelayAfterAction(1f));
+        }
+
+        private IEnumerator DelayAfterAction(float time)
+        {
+            yield return new WaitForSeconds(time);
+            NewActionStart();
+        }
+    }
+}
---
+++ Assets/scripts/AI/AIVisualSignalSystem.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIVisualSignalSystem : MonoBehaviour
+{
+    public GameObject AttackSignal;
+
+    private RectTransform _signalTransform;
+    private Image _image;
+    private Color _startColor;
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _image = AttackSignal.GetComponent<Image>();
+        _signalTransform = AttackSignal.GetComponent<RectTransform>();
+        _startColor = _image.color;
+        _startPosition = _signalTransform.transform.position;
+        
+    }
+
+    public void PlaceAttackSignal(int actionIndex)
+    {
+        if (actionIndex>4)
+        {
+            _image.color = Color.blue;
+            switch (actionIndex)
+            {
+                case 5:
+                    _signalTransform.transform.localPosition = new Vector3(30, 100, -1);
+                    break;
+                case 6:
+                    _signalTransform.transform.localPosition = new Vector3(-30, 100, -1);
+                    break;
+            }
+        }
+        else
+        {
+            switch (actionIndex)
+            {
+                    case 1:
+                        _signalTransform.transform.localPosition = new Vector3(250,130,-1);
+                        break;
+                    case 2:
+                        _signalTransform.transform.localPosition = new Vector3(-250,130,-1);
+                        break;
+                    case 3:
+                        _signalTransform.transform.localPosition = new Vector3(250,-130,-1);
+                        break;
+                    case 4:
+                        _signalTransform.transform.localPosition = new Vector3(-250,-130,-1);
+                        break;
+            }
+        }
+
+        var imageColor = _image.color;
+        imageColor.a = 1f;
+        _image.color = imageColor;
+        StartCoroutine(DelayBeforeReset(0.5f));
+    }
+
+    private IEnumerator DelayBeforeReset(float time)
+    {
+        var imageColor = _image.color;
+        imageColor.a -= 0.2f;
+        _image.color = imageColor;
+        yield return new WaitForSeconds(time);
+        ResetImageToDefault();
+    }
+
+    private void ResetImageToDefault()
+    {
+        _signalTransform.transform.localPosition = _startPosition;
+        _image.color = _startColor;
+    }
+}
