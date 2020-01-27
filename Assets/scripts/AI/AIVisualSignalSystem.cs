b9d805caffda0f7a3f84249bb8378ad90420ad39
Danil Bogodvid <d.bogodvid@kefirgames.com>
2020-01-27 16:53:44 +0400

    -fix: AIVisualSignalSystem;

--- Assets/scripts/AI/AIVisualSignalSystem.cs
+++ Assets/scripts/AI/AIVisualSignalSystem.cs
@@ -4,74 +4,70 @@ using UnityEngine.UI;
 
 public class AIVisualSignalSystem : MonoBehaviour
 {
-    public GameObject AttackSignal;
-
-    private RectTransform _signalTransform;
-    private Image _image;
+    public RectTransform SignalTransform;
+    public Image Image;
     private Color _startColor;
     private Vector3 _startPosition;
 
-    private void Start()
+    private void Awake()
     {
-        _image = AttackSignal.GetComponent<Image>();
-        _signalTransform = AttackSignal.GetComponent<RectTransform>();
-        _startColor = _image.color;
-        _startPosition = _signalTransform.transform.position;
-        
+        _startColor = Image.color;
+        _startPosition = SignalTransform.transform.position;
     }
 
     public void PlaceAttackSignal(int actionIndex)
     {
         if (actionIndex>4)
         {
-            _image.color = Color.blue;
+            Image.color = Color.blue;
             switch (actionIndex)
             {
                 case 5:
-                    _signalTransform.transform.localPosition = new Vector3(30, 100, -1);
+                    SignalTransform.transform.localPosition = new Vector3(30, 100, 49);
                     break;
                 case 6:
-                    _signalTransform.transform.localPosition = new Vector3(-30, 100, -1);
+                    SignalTransform.transform.localPosition = new Vector3(-30, 100, 49);
                     break;
             }
         }
         else
         {
+            Image.color = Color.red;
             switch (actionIndex)
             {
                     case 1:
-                        _signalTransform.transform.localPosition = new Vector3(250,130,-1);
+                        SignalTransform.transform.localPosition = new Vector3(250,130,49);
                         break;
                     case 2:
-                        _signalTransform.transform.localPosition = new Vector3(-250,130,-1);
+                        SignalTransform.transform.localPosition = new Vector3(-250,130,49);
                         break;
                     case 3:
-                        _signalTransform.transform.localPosition = new Vector3(250,-130,-1);
+                        SignalTransform.transform.localPosition = new Vector3(250,-130,49);
                         break;
                     case 4:
-                        _signalTransform.transform.localPosition = new Vector3(-250,-130,-1);
+                        SignalTransform.transform.localPosition = new Vector3(-250,-130,49);
                         break;
             }
         }
 
-        var imageColor = _image.color;
+        var imageColor = Image.color;
         imageColor.a = 1f;
-        _image.color = imageColor;
+        Image.color = imageColor;
         StartCoroutine(DelayBeforeReset(0.5f));
     }
 
     private IEnumerator DelayBeforeReset(float time)
     {
-        var imageColor = _image.color;
+        var imageColor = Image.color;
         imageColor.a -= 0.2f;
-        _image.color = imageColor;
+        Image.color = imageColor;
         yield return new WaitForSeconds(time);
         ResetImageToDefault();
     }
 
     private void ResetImageToDefault()
     {
-        _signalTransform.transform.localPosition = _startPosition;
-        _image.color = _startColor;
+        SignalTransform.transform.localPosition = new Vector3(0,0,49);
+        Image.color = _startColor;
     }
 }
